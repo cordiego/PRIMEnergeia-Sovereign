9c8f39ecc74e6397c4af23d06a0f38a58d7bae0a
cordiego <cordobaurrutia95@gmail.com>
2026-03-23 12:44:36 -0600

    Load and modify dashboard configuration in Eurekaadd Eureka page wrapper

---
+++ pages/2_Eureka_Sovereign.py
@@ -0,0 +1,7 @@
+import sys, os
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+_p = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), "dashboard", "dashboard_eureka.py")
+with open(_p) as f:
+      _c = f.read()
+  _c = _c.replace('st.set_page_config(', '# st.set_page_config(').replace('initial_sidebar_state="collapsed"\n)', 'initial_sidebar_state="collapsed"#)')
+exec(_c)
