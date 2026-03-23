35a36e9427eb7ff01ff0a60ac2ccc7439725bc94
cordiego <cordobaurrutia95@gmail.com>
2026-03-23 13:28:59 -0600

    Refactor import and file reading in Granas Optimizer

--- pages/3_Granas_Optimizer.py
+++ pages/3_Granas_Optimizer.py
@@ -1,8 +1,6 @@
-import sys, os
+import sys, os, re
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 _p = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), "optimization", "granas_dashboard.py")
-with open(_p) as f:
-          _c = f.read()
-      import re
+_c = open(_p).read()
 _c = re.sub(r'st\.set_page_config\s*\(.*?\)', 'pass', _c, flags=re.DOTALL)
 exec(_c)
